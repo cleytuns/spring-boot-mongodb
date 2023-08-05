f6fbfd25c75b63b4e4a11a3bd3113d4ba2deaa77
cleytuns <cleyto.nunes613@gmail.com>
2023-08-05 15:11:20 -0300

    Obtendo um usuario por id

--- src/main/java/com/cleytuns/mongo/services/UserService.java
+++ src/main/java/com/cleytuns/mongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.cleytuns.mongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cleytuns.mongo.domain.User;
 import com.cleytuns.mongo.repository.UserRepository;
+import com.cleytuns.mongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,12 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		
 	}
 
 }
