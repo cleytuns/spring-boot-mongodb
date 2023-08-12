50f66ea4fe578bcf928e53fd54888b61decfd595
cleytuns <cleyto.nunes613@gmail.com>
2023-08-12 13:49:31 -0300

    Obtendo um post por id

---
+++ src/main/java/com/cleytuns/mongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.cleytuns.mongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cleytuns.mongo.domain.Post;
+import com.cleytuns.mongo.repository.PostRepository;
+import com.cleytuns.mongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+		
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
