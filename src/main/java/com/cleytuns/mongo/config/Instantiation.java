8ad15433a594dd2ab67ab87c96b78fec4698236b
cleytuns <cleyto.nunes613@gmail.com>
2023-08-12 13:18:00 -0300

    Projecao dos dados do Autor com DTO

--- src/main/java/com/cleytuns/mongo/config/Instantiation.java
+++ src/main/java/com/cleytuns/mongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.cleytuns.mongo.domain.Post;
 import com.cleytuns.mongo.domain.User;
+import com.cleytuns.mongo.dto.AuthorDTO;
 import com.cleytuns.mongo.repository.PostRepository;
 import com.cleytuns.mongo.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2023"), "Partiu viagem", "Vou viajar para São Paulo, abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2023"), "Bom dia", "Acordei feliz hoje", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2023"), "Partiu viagem", "Vou viajar para São Paulo, abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2023"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
